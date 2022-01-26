231bdc3664311a7d1f73ceecf36e2e849f6dad6b
tawfiq khalilieh <87164562+xtawfeeq7k@users.noreply.github.com>
2022-01-26 16:52:01 +0200

    Create api.py

---
+++ api.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI,Path
+from pydantic import BaseModel
+app = FastAPI()
+
+employees = {
+    1: {
+        "name": "tawfeeq" ,
+        "email": "tawfeeq@gmail.com",
+        "major": "BackEnd web dev",
+        "salary": 999999999
+    }
+}
+class Employee(BaseModel):
+    name: str
+    email: str
+    major: str
+    salary: int
+class UpdateEmployee(BaseModel):
+    name: str = None
+    email: str = None
+    major: str = None
+    salary: int = None
+
+@app.get("/get-employees")
+def getAll():
+    return employees
+
+@app.get("/get-employee/{id}")
+def get_student(id: int = Path(None,description="The ID of the employee you want to view" , gt = 0)):
+    return employees[id]
+
+@app.get("/get-by-name")
+def get_employee(name: str = None):
+    for id in employees:
+        if employees[id]["name"] == name:
+            return employees[id]
+    return{"Data": "Not Found"}
+
+@app.post("/create-employee/{id}")
+def createStudent(id: int , employee : Employee):
+    if id in employees:
+        return{"Error": {"Employee exists"}}
+    employees[id] = employee
+    return employees[id]
+
+@app.put("/update-employee/{id}")
+def UpdateEmployee(student_id: int , employee: UpdateEmployee):
+    if id in employees:
+        return {"Error": "This Student does not exist"}
+    employees[id]=employee
+    return employees[id]
